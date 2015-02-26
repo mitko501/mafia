2cf183ca42b83446882bb51dc5da68418283708b
mitko501 <miko501@azet.sk>
2015-02-26 21:35:46 +0100

    new version of url parser

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
 <?php
 ob_start();
 require_once('/config/define.php');
-require_once(BASE_DIR . 'libs/FirePHPCore/FirePHP.class.php');
-require_once(BASE_DIR . 'libs/FirePHPCore/fb.php');
-require_once(BASE_DIR . 'registry/MySQL.class.php');
 require_once(BASE_DIR . 'registry/registry.php');
 
-$registry= new Registry();
+$registry = new Registry();
 $registry->setDebugging(true);//DEBUG
-$registry->firephp->log('index.php: start logging');
+$registry->getFirePHP()->log('index.php: start logging');
 $registry->getObject('MySQL')->newConnection('');
 
-$controller = $registry->getObject('url')->getUrlBit(0);
+$controller = $registry->getObject('Url')->getController();
 include(BASE_DIR . 'controllers/' . $controller . '/controller.php' );
 $controller = $controller.'Controller';
 new $controller($registry);
-$registry->firephp->log('End of PHP.');
+$registry->getFirePHP()->log('End of PHP.');
 ?>
